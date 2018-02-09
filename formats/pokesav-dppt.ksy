39f9c7d9e44f7135e874ab776de5ff174d169d7d
Raymond Hammarling <raymond@hammarlings.se>
2018-02-09 10:24:18 +0100

    Implement compiling KSY file

---
+++ formats/pokesav-dppt.ksy
@@ -0,0 +1,9 @@
+meta:
+  id: pokesav_dppt
+  endian: le
+  file-extension: sav
+  application: The Pokemon Company
+
+seq:
+  - id: placeholder_thing
+    type: u2
---
+++ scripts/build-kaitai-format.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const fse = require('fs-extra');
+const yaml = require('js-yaml');
+const KaitaiStructCompiler = require('kaitai-struct-compiler');
+
+async function readKsyFile(path) {
+  const ksyStr = await fse.readFile(path, { encoding: 'utf8' });
+
+  return yaml.safeLoad(ksyStr, { filename: path });
+}
+
+async function main() {
+  const ksyData = await readKsyFile(
+    path.join(__dirname, '../formats/pokesav-dppt.ksy')
+  );
+
+  const compiler = new KaitaiStructCompiler();
+
+  const files = await compiler.compile('javascript', ksyData, null, false);
+
+  await fse.mkdirp(
+    path.join(__dirname, '../formats-compiled')
+  );
+
+  await Promise.all(
+    Object.keys(files).map(async file => {
+      return await fse.writeFile(
+        path.join(__dirname, '../formats-compiled', file),
+        files[file],
+        { encoding: 'utf8' }
+      );
+    })
+  );
+
+  console.log(`Compiled files: ${Object.keys(files).join(', ')}`);
+}
+
+main()
+  .catch(err => console.error(err.stack));
