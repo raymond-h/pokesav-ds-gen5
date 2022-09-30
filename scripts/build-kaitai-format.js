8b14f8973628274d4400b464554a27c552aecc53
Raymond Hammarling <raymond@hammarlings.se>
2022-09-30 19:52:16 +0200

    Replace UMD in compiled KSY with just CommonJS

--- scripts/build-kaitai-format.js
+++ scripts/build-kaitai-format.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const assert = require('assert/strict');
 const fse = require('fs-extra');
 const yaml = require('js-yaml');
 const falafel = require('falafel');
@@ -14,17 +15,47 @@ async function readKsyFile(path) {
   return yaml.safeLoad(ksyStr, { filename: path });
 }
 
+function findInIfElseIfChainSyntaxTree(node, pred) {
+  assert.equal(node.type, 'IfStatement');
+
+  if (pred(node.consequent)) {
+    return node.consequent;
+  }
+  else if (node.alternate != null && node.alternate.type === 'IfStatement') {
+    return findInIfElseIfChainSyntaxTree(node.alternate, pred);
+  }
+  else if (node.alternate != null) {
+    return pred(node.alternate) ? node.alternate : null;
+  }
+}
+
 function transformJs(src) {
   return falafel(src, node => {
-    if(node.type === 'CallExpression' &&
+    if (node.type === 'CallExpression' &&
       node.callee.type === 'Identifier' &&
       node.callee.name === 'require' &&
       node.arguments[0].type === 'Literal') {
 
       const match = /^pokesav[\\/](.+)$/.exec(node.arguments[0].value);
 
-      if(match != null) {
+      if (match != null) {
         node.update(`require('../lib/decrypt-process.js').${match[1]}`);
+      }
+    }
+
+    if (node.type === 'FunctionExpression' &&
+      node.id === null &&
+      node.params.length === 2 &&
+      node.params[0].name === 'root' &&
+      node.params[1].name === 'factory') {
+
+      const cjsBlock = findInIfElseIfChainSyntaxTree(node.body.body[0], (node) => {
+        const expr = node.body[0].expression;
+        return expr.type === 'AssignmentExpression' && expr.operator === '=' && expr.left.source() === 'module.exports';
+      });
+
+      if (cjsBlock != null) {
+        node.body.body[0].update(cjsBlock.body[0].source());
       }
     }
   });
@@ -55,7 +86,7 @@ async function compileFile(file, outputDir) {
 async function main(argv) {
   let files = argv._;
 
-  if(argv.source) {
+  if (argv.source) {
     const globFiles = await globAsync(argv.source);
     files = files.concat(globFiles || []);
   }
